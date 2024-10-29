26a0be517bf5e0b3da227da1b313e4ac2a1ae83d
rishabhshukla <rishabh.shukla8@gmail.com>
2024-10-29 23:12:08 +0530

    Added the merging of two sorted array

--- M04-Advanced DSA1/M08-DSA: Sorting 1: Merge Sort.py
+++ M04-Advanced DSA1/M08-DSA: Sorting 1: Merge Sort.py
@@ -5,4 +5,40 @@ def start_new(func_name):
     print(f"Proessing for : {func_name}!!!\n")
 
 
+def merge_Sort(A,B):
+    print(A,B)
+    n ,m = len(A) , len(B)
+    res = [0] * (n+m)
+    print(res)
+
+    i,j,k = 0,0,0
+    # using two pointers approach
+    while i < n and j < m:
+        if A[i] <= B[j]:
+            res[k] = A[i]
+            i += 1
+            k += 1
+        else :
+            res[k] = B[j]
+            k += 1
+            j += 1
+    
+    while i < n :
+        res[k] = A[i]
+        i += 1
+        k += 1
+    
+    while j < m :
+        res[k] = B[j]
+        k += 1
+        j += 1
+    
+    print(f"Merger sorted array is : {res}")
+
+
+
 start_new("Merge sort !!!")
+
+A = [2,4,7,8,12]
+B = [3,5,6,7]
+merge_Sort(A,B)
