a07e3826181ca7df9e07d512c02bcf07b62a3cc4
rishabhshukla <rishabh.shukla8@gmail.com>
2024-10-29 22:13:17 +0530

    Added count sort with -ve values

--- M04-Advanced DSA1/M08-DSA: Sorting 1: Count Sort & Merge Sort.py
+++ M04-Advanced DSA1/M08-DSA: Sorting 1: Count Sort & Merge Sort.py
@@ -36,3 +36,34 @@ start_new("Smallest Number Using the Array")
 A = [6,3,4,2,7,2,1]
 smallest_number(A)
 
+
+start_new("Smallest Number  Array with -ve values")
+
+def smallest_no_negative(arr):
+    # get length of freq array
+    # create freq array - with value from the main array
+
+    n = len(arr)
+
+    min_v = min(arr)
+    max_v = max(arr)
+
+    freq_length = max_v - min_v + 1
+    freq = [0]*freq_length
+    print(f"max min freq {max_v , min_v , freq}")
+
+    for i in range(n):
+        freq[arr[i] - min_v] += 1
+    print(freq)
+
+    k = 0 
+    for i in range(freq_length):
+        temp_freq = freq[i]
+        for j in range(temp_freq):
+            arr[k] = i + min_v
+            k += 1
+    
+    print(arr)
+
+A = [-2,3,8,3,-2,3]
+smallest_no_negative(A)
